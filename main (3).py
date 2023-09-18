4dfa826233377149ec4434309d28224d53be6674
SanthoshSanthoshrtr <145350633+SanthoshSanthoshrtr@users.noreply.github.com>
2023-09-18 18:51:45 +0530

    Challenge Unit 1.1

---
+++ main (3).py
@@ -0,0 +1,11 @@
+#implement a recursive function to calculate the factorial of a given number
+def factorial(n):
+    if (n == 1 or n == 0):
+        return 1
+    else:
+        return (n * factorial(n - 1)) 
+
+# Driver Code
+num = 5
+print("number:", num)
+print("Factorial:", factorial(num))
